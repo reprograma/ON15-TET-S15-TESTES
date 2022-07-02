c433873579808fff7817a84e3388a4464cc6a69a
elizapimentel <elizapimentel@hotmail.com>
2022-07-02 17:45:16 -0300

    :sparkles: add teste patch

--- src/test/api.test.js
+++ src/test/api.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 const app = require('../app');
 
+let elementId //var criada pra receber o id criado
+
 //agrupa vários testes
 describe("API test", () => {
     //testar get all
@@ -30,10 +32,32 @@ describe("API test", () => {
             .expect(201)
             .end((err, res) =>{
                 if(err) return done(err)
+                //recebe a reatribuição com o id gerado na criação
+                elementId = res.body.savedUser._id
                 return done()
             })
     })
-    
+    test("PATCH /users/update/:id", (done) => {
+        request(app)
+            .patch(`/users/update/${elementId}`)
+            .expect("Content-Type", /json/)
+            .send({
+                name: "Eliza Pimentel",
+                email: "eliza12@email.com"
+            })
+            .expect(200)
+            //conferir se foi atualizado
+            .expect((res) => {
+                expect(res.body.savedUser._id).toBe(elementId)
+                expect(res.body.savedUser.name).toBe("Eliza Pimentel")
+                expect(res.body.savedUser.email).toBe("eliza12@email.com")
+            })
+            .end((err, res) => {
+                if(err) return done(err)
+                return done()
+            })
+    })
+
 
 })
    
