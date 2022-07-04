e3441b7e37002d90ebb6f42610cb430424a98cf7
anaelisaq <anaelisa.abs@gmail.com>
2022-07-04 00:48:51 -0300

    :test_tube: test

--- src/test/api.test.js
+++ src/test/api.test.js
@@ -1,77 +1,75 @@
-const request = require("supertest");
-const app = require("../app");
+const request = require("supertest")
+const app = require("../app")
 
-//jest.setTimeout(30500);
-//jest.useRealTimers();
+// jest.setTimeout(30500)
+// jest.useRealTimers()
 
-let elementId;
+let elementId
 
 describe("API test", () => {
-    // agrupa vários testes
-    // testar primeiro a rota GET
-
     test("GET /users/all", (done) => {
-        request(app)
-            .get("/users/all")
-            .expect(200)
+        request(app) //realizar conexão
+            .get("/users/all") //fazer um get
+            .expect(200) //expectativa do resultado
             .expect((res) => {
-                expect(res.body.length).not.toBe(0);
+                console.log("body da resposta", res.body)
+                expect(res.body.length).not.toBe(0)
             })
             .end((err, res) => {
-                if (err) return done(err);
-                return done();
-            });
-    });
+                if(err) return done(err)
+                return done()
+            })
+    })
 
     test("POST /users/create", (done) => {
         request(app)
             .post("/users/create")
-            .expect("Content-Type", /json/)
+            .expect("Content-type", /json/)
             .send({
-                name: "Paula",
-                email: "paula@email.com",
-                password: "senhamuitodificil"
+                name: "Alguém",
+                email: "alguem@email",
+                password: "senhadificil"
             })
             .expect(201)
             .end((err, res) => {
-                if(err) return done(err);
-                elementId = res.body.savedUser._id;
-                return done();
-            });
-    });
+                if(err) return done(err)
+                elementId = res.body.savedUser._id
+                return done()
+            })
+    })
 
-    test("PATCH /users/update/:id", (done) => {
+    test("PATCH /users/update/:id", (done) =>{
         request(app)
             .patch(`/users/update/${elementId}`)
-            .expect("Content-Type", /json/)
+            .expect("Content-type", /json/)
             .send({
-                name: "Paula Atualizado",
-                email: "paulaatualizado@email.com"
+                name: "testando",
+                email: "testando@email"
             })
             .expect(200)
-            .expect((res) =>{
-                expect(res.body.savedUser._id).toBe(elementId);
-                expect(res.body.savedUser.name).toBe("Paula Atualizado");
-                expect(res.body.savedUser.email).toBe("paulaatualizado@email.com");
+            .expect((res) => {
+                expect(res.body.savedUser._id).toBe(elementId)
+                expect(res.body.savedUser.name).toBe("testando")
+                expect(res.body.savedUser.email).toBe("testando@email")
             })
             .end((err, res) => {
-                if(err) return done(err);
-                return done();
-            });
-    });
+                if(err) return done(err)
+                return done()
+            })
+    })
 
     test("DELETE /users/delete/:id", (done) => {
         request(app)
-          .delete(`/users/delete/${elementId}`)
-          .expect("Content-Type", /json/)
-          .expect(200)
-          .expect((res) => {
-            console.log(res.body)
-            expect(res.body.userFound.email).toBe("paulaatualizado@email.com");
-          })
-          .end((err, res) => {
-            if (err) return done(err);
-            return done();
-          });
-      });
-});
+            .delete(`/users/delete/${elementId}`)
+            .expect("Content-Type", /json/)
+            .expect(200)
+            .expect((res) => {
+                console.log(res.body)
+                expect(res.body.userFound.email).toBe("paulaatualizado@email.com");
+            })
+            .end((err, res) => {
+                if (err) return done(err);
+                return done();
+            })
+    })
+})
