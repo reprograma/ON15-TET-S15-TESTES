62853401c23aacb82c9f1ffaaea06cc6d775d935
elizapimentel <elizapimentel@hotmail.com>
2022-07-05 22:48:36 -0300

    :fire: del store routes

--- para-o-lar/app.js
+++ para-o-lar/app.js
@@ -10,15 +10,13 @@ app.use(express.json());
 const db = require('./src/config/database');
 db.connect();
 
-const clientRoutes = require('./src/routes/clientsRouts');
-const ordersRoutes = require('./src/routes/ordersRouts');
+const clientRoutes = require('./src/routes/clientsRoutes');
+const ordersRoutes = require('./src/routes/ordersRoutes');
 const userRoutes = require('./src/routes/userRoutes');
-const storeRoutes = require('./src/routes/storesRoutes');
 
 app.use("/client", clientRoutes);
 app.use("/order", ordersRoutes);
 app.use("/user", userRoutes);
-app.use("/store", storeRoutes);
 
 module.exports = app;
 
