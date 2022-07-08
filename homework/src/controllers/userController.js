992ccb3a709b2afd04c98cf899d836b6c927aaa5
elizapimentel <elizapimentel@hotmail.com>
2022-07-08 12:17:29 -0300

    :recycle: including return getall func

--- homework/src/controllers/userController.js
+++ homework/src/controllers/userController.js
@@ -1,13 +1,7 @@
 const UserSchema = require('../models/userSchema')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-
-const SECRET = process.env.SECRET
 
 const createUser = async (req, res) => {
-    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-    req.body.password = hashedPassword      
-
     try {
         const user = new UserSchema({
             name: req.body.name,
@@ -32,13 +26,13 @@ const createUser = async (req, res) => {
     }
 }
 
-const getAll = async (req, res) => {
-    
+const getAll = async (req, res) => {    
     UserSchema.find(function (err, users) {
         if(err) {
-          res.status(500).send({ message: err.message })
+          return res.status(500).send({ 
+            message: err.message })
         }
-          res.status(200).send(users)
+        return res.status(200).json(users)
     }) 
 }
 
@@ -56,24 +50,6 @@ const updateUser = async (req, res) => {
 }
 
 
-const updatePassword = async (req, res) => {
-    const user = await UserSchema.findById(req.params.id)
-    if(!user) {
-        return res.status(404).send({ message: "Usuário não encontrado" })
-    }
-
-    console.log("Senha inicial",req.body.password)
-    const newHashedPassword = bcrypt.hashSync(req.body.password, 10);
-    req.body.password = newHashedPassword      
-    console.log("Senha hasheada",req.body.password)
-    console.log("Senha anterior",user.password)
-    user.password = req.body.password
-    console.log("Senha final", user.password)
-
-    const updatedUser = await user.save()
-    res.status(200).send(updatedUser)
-}
-
 const deleteUser = async (req, res) => {
     const user = await UserSchema.findByIdAndDelete(req.params.id)
     if(!user) {
@@ -87,6 +63,5 @@ module.exports = {
     createUser,
     getAll,
     updateUser,
-    updatePassword,
     deleteUser
 }
