eef1a1b7657587ede57798eee44ab5af3bc0736c
elizapimentel <elizapimentel@hotmail.com>
2022-07-08 18:07:33 -0300

    update routes

--- homework/src/app.js
+++ homework/src/app.js
@@ -1,19 +1,18 @@
 const express = require('express');
 const app = express();
-
 const cors = require('cors');
-app.use(cors());
 
 require('dotenv-safe').config();
-app.use(express.json());
 
 const db = require('./config/database');
-db.connect();
-
 const clientRoutes = require('./routes/clientsRoutes');
 const ordersRoutes = require('./routes/ordersRoutes');
 const userRoutes = require('./routes/userRoutes');
 
+db.connect();
+
+app.use(cors());
+app.use(express.json());
 app.use("/client", clientRoutes);
 app.use("/order", ordersRoutes);
 app.use("/user", userRoutes);
