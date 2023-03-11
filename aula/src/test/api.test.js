5da1b9c7f1bb4f8a03aee771d9580f610999ead0
brena-odwyer <odwyer.brena@gmail.com>
2023-03-11 16:48:03 -0500

    subindo anotações antigas

--- aula/src/test/api.test.js
+++ aula/src/test/api.test.js
@@ -1,5 +1,7 @@
-const request = require("supertest"); // a const chama request pq o supertest permite a requisição; o supertest usa o jest pra fazer os testes
-const app = require("../app"); // importamos o app.js porque lá temos a const app = express() que armazena o express, e usamos esse app para fazer a conexão do servidor e aqui ele tem a mesma responsabilidade
+const request = require("supertest"); // a const chama request pq o supertest permite a requisição; 
+//o supertest usa o jest pra fazer os testes
+const app = require("../app"); // importamos o app.js porque lá temos a const app = express() 
+//que armazena o express, e usamos esse app para fazer a conexão do servidor e aqui ele tem a mesma responsabilidade
 
 // jest.setTimeout(30500);
 
@@ -8,12 +10,15 @@ let elementId; //pra fazer update e delete precisamos do id
 describe("API test", () => {  //agrupa vários testes
     // testar primeiro a rota GET
 
-    test("GET /users/all", (done) => { //descrição do teste unitário como primeiro parâmetro; função anônima como segundo no done
+    test("GET /users/all", (done) => { //descrição do teste unitário como primeiro parâmetro; 
+        //função anônima como segundo no done
          request(app) // conexão acontece
             .get("/users/all") //teste fazer um GET, é a requisição
-            .expect(200) // Uma função que diz o que esperamos que aconteça, espero que o status seja 200 e não 500, validação do status code
+            .expect(200) // Uma função que diz o que esperamos que aconteça, 
+            //espero que o status seja 200 e não 500, validação do status code
             .expect((res)=>{
-                // console.log("body da resposta", res.body); // mostra no terminal todos os usuarios cadastrados no banco
+                // console.log("body da resposta", res.body); 
+                // mostra no terminal todos os usuarios cadastrados no banco
                 expect(res.body.length).not.toBe(0) //Aqui dentro é lógica de js
 
             })
@@ -34,7 +39,8 @@ describe("API test", () => {  //agrupa vários testes
             })
             .expect(201) //teste do status
             .end((err, res) => {
-                // console.log("body da resposta", res.body) //os consoles são pra gente ver a res.body mudando
+                // console.log("body da resposta", res.body) 
+                //os consoles são pra gente ver a res.body mudando
                 if(err) return done(err); // se o if tem um único retorno o uso de chaves é facultativo
                 // console.log("id do usuário recem-criado", res.body.savedUser._id)
                 elementId = res.body.savedUser._id;
@@ -81,16 +87,15 @@ describe("API test", () => {  //agrupa vários testes
 
 
 });
-
-
-
 /*
 Funções JEST
 
 describe() - agrupa um conjunto de testes individuais
 Recebe dois parâmetros, o nome dos testes e uma função anônima
 
-test() ou it() - fazem a mesma coisa, realizam um teste individual, vamos usar o test na aula por ser mais semântico
-// primeiro parâmetro é o endpoint: o verbo e a rota, precisa da barra antes, precisa da rota raíz e da rota do verbo
+test() ou it() - fazem a mesma coisa, realizam um teste individual
+vamos usar o test na aula por ser mais semântico
+// primeiro parâmetro é o endpoint: o verbo e a rota, precisa da barra antes, 
+precisa da rota raíz e da rota do verbo
 o done ali é quando o teste acaba
 */
